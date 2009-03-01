0a2d536d1b0c2328a8218acc805cbc01f6c8de3f
Joshua Kuhn <habitue@krishna.(none)>
2009-03-01 00:40:32 -0500

    Made all commands in the loop work correctly

--- dm_initiative.py
+++ dm_initiative.py
@@ -1,13 +1,16 @@
+try:
+    import readline
+except:
+    pass
 from dm_common import *
 
 def mml(mon_type, num, init_mod):
     "Quickly creates a list of monsters of the given quantity"
     mon_list = []
-    for i in xrange(num):
-        mon = Player(mon_type + " " + chr(ord('A')+i), init_mod)
+    for pl in xrange(num):
+        mon = Player(mon_type + " " + chr(ord('A')+pl), init_mod)
         mon_list.append(mon)
     return mon_list
-
 
 
 class Encounter(object):
@@ -36,3 +39,91 @@ class Encounter(object):
         for (num,name) in self.init_list:
             ret_string += str(num) + " -> " + name + "\n"
         return ret_string
+
+def new_monsters():
+    "Creates new monsters from the command line"
+    ret_list = []
+    more = "yes"
+    while more != "no":
+        mon_name = raw_input("Monster name: ").title()
+        mon_num = int(raw_input("Number of %s(s): " % mon_name))
+        mon_init = int(raw_input("%s initiative: " % mon_name))
+        ret_list.extend(mml(mon_name, mon_num, mon_init))
+        more = raw_input("More monster types? (yes/no)").strip().lower()
+    return ret_list
+    
+def new_encounter(plist, mlist):
+    p_init = int(raw_input("Enter situational init modifier"\
+                               " for the players:"))
+    m_init = int(raw_input("Enter situational init modifier"\
+                               " for the monsters:"))
+    enc = Encounter(plist, mlist, p_init, m_init)
+    print "Ok, here is the encounter initiative order:"
+    print enc
+    return enc
+
+class Completer(object):
+    def __init__(self, options):
+        self.options = sorted(options)
+        self.matches = []
+        
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            if text:
+                self.matches = [s
+                                for s in self.options
+                                if s and s.startswith(text)]
+            else:
+                self.matches = self.options[:]
+        
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        return response
+    
+
+if __name__ == "__main__":
+    print "-- Initiative Helper --".center(80)
+    num_players = int(raw_input("How many players are there? "))
+    player_list = []
+    for i in xrange(num_players):
+        name = raw_input("Player's name: ")
+        init = int(raw_input("%s's initiative modifier: " % name))
+        player_list.append(Player(name, init))
+
+    monster_list = []
+    encounter = None
+    command_list = ['help','new_monsters','new_encounter', 'print_init',
+                    'exit']
+    completer = Completer(command_list)
+    print 'type "help" for help'
+    readline.set_completer(completer.complete) # add tab completion options
+    readline.parse_and_bind("tab: complete") # add tab completion
+    while True:
+        inn = raw_input("Command : ").strip().lower()
+        if inn == "help":
+            print "Commands: ",
+            for s in command_list:
+                print s + ",",
+            print ""
+        elif inn == "new_monsters":
+            monster_list = new_monsters()
+        elif inn == "new_encounter":
+            encounter = new_encounter(player_list, monster_list)
+        elif inn == "save":
+            if encounter:
+                savefile(encounter)
+            else:
+                print "Nothing to save."
+        elif inn == "print_init":
+            try:
+                print encounter
+            except:
+                print 'No encounter yet, type "new_encounter"'
+        elif inn == "exit":
+            print "Bye"
+            break
+        else:
+            print "No such command"
