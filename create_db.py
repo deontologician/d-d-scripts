17e91065f8fb7419ec5347e9a7d2b777a8c3d85b
Josh Kuhn <deontologician@gmail.com>
2014-02-19 23:50:48 -0500

    More models, more relationships. Started on effect system

--- create_db.py
+++ create_db.py
@@ -27,6 +27,30 @@ def initialize_database(session):
         M.Size(name='Gargantuan', space=4),
     ])
     session.add_all([
+        M.DamageType('Acid',
+                     'Corrosive liquid'),
+        M.DamageType('Cold',
+                     'Ice crystals, arctic air, or frigid liquid'),
+        M.DamageType('Fire',
+                     'Explosive bursts, fiery rays, or simple ignition'),
+        M.DamageType('Force',
+                     'Invisible energy formed into incredibly hard, '
+                     'yet nonsolid shapes'),
+        M.DamageType('Lightning',
+                     'Electrical energy'),
+        M.DamageType('Necrotic',
+                     'Purple-black energy that deadens flesh '
+                     'and wounds the soul'),
+        M.DamageType('Poison',
+                     "Toxins that reduce a creature's hit points"),
+        M.DamageType('Psychic',
+                     'Effects that target the mind'),
+        M.DamageType('Radiant',
+                     'Searing white light or shimmering colors'),
+        M.DamageType('Thunder',
+                     'Shock waves and deafening sounds'),
+    ])
+    session.add_all([
         M.Alignment(name='Lawful Good', good=True, lawful=True),
         M.Alignment(name='Good', good=True),
         M.Alignment(name='Unaligned'),
@@ -34,22 +58,23 @@ def initialize_database(session):
         M.Alignment(name='Chaotic Evil', evil=True, chaotic=True),
     ])
     session.add_all([
+        # Default surges = 7
         M.Class('Cleric', 'Divine', 'Leader'),
-        M.Class('Fighter', 'Martial', 'Defender'),
-        M.Class('Paladin', 'Divine', 'Defender'),
-        M.Class('Ranger', 'Martial', 'Striker'),
-        M.Class('Rogue', 'Martial', 'Striker'),
-        M.Class('Warlock', 'Arcane', 'Striker'),
+        M.Class('Fighter', 'Martial', 'Defender', surges=9),
+        M.Class('Paladin', 'Divine', 'Defender', surges=10),
+        M.Class('Ranger', 'Martial', 'Striker', surges=6),
+        M.Class('Rogue', 'Martial', 'Striker', surges=6),
+        M.Class('Warlock', 'Arcane', 'Striker', surges=6),
         M.Class('Warlord', 'Martial', 'Leader'),
-        M.Class('Wizard', 'Arcane', 'Controller'),
+        M.Class('Wizard', 'Arcane', 'Controller', surges=6),
         M.Class('Avenger', 'Divine', 'Striker'),
-        M.Class('Barbarian', 'Primal', 'Striker'),
+        M.Class('Barbarian', 'Primal', 'Striker', surges=8),
         M.Class('Bard', 'Arcane', 'Leader'),
         M.Class('Druid', 'Primal', 'Controller'),
-        M.Class('Invoker', 'Divine', 'Controller'),
+        M.Class('Invoker', 'Divine', 'Controller', surges=6),
         M.Class('Shaman', 'Primal', 'Leader'),
-        M.Class('Sorcerer', 'Arcane', 'Striker'),
-        M.Class('Warden', 'Primal', 'Defender'),
+        M.Class('Sorcerer', 'Arcane', 'Striker', surges=6),
+        M.Class('Warden', 'Primal', 'Defender', surges=9),
     ])
     session.add_all([
         M.Race(name='Dragonborn'),
@@ -159,6 +184,37 @@ def initialize_database(session):
         M.Deity(name='Zehir',
                 alignmentname='Evil',
                 domains=['Darkness', 'Poison']),
+    ])
+    session.add_all([
+        M.Skill('Acrobatics', M.DEX, armor_penalty=True),
+        M.Skill('Arcana', M.INT),
+        M.Skill('Athletics', M.STR, armor_penalty=True),
+        M.Skill('Bluff', M.CHA),
+        M.Skill('Diplomacy', M.CHA),
+        M.Skill('Dungeoneering', M.WIS),
+        M.Skill('Endurance', M.CON, armor_penalty=True),
+        M.Skill('Heal', M.WIS),
+        M.Skill('History', M.INT),
+        M.Skill('Insight', M.WIS),
+        M.Skill('Intimidate', M.CHA),
+        M.Skill('Nature', M.WIS),
+        M.Skill('Perception', M.WIS),
+        M.Skill('Religion', M.INT),
+        M.Skill('Stealth', M.DEX, armor_penalty=True),
+        M.Skill('Streetwise', M.CHA),
+        M.Skill('Thievery', M.DEX, armor_penalty=True),
+    ])
+    session.add_all([
+        M.Language(name='Common', script='Common'),
+        M.Language(name='Deep Speech', script='Rellanic'),
+        M.Language(name='Draconic', script='Iokharic'),
+        M.Language(name='Dwarven', script='Davek'),
+        M.Language(name='Elven', script='Rellanic'),
+        M.Language(name='Giant', script='Davek'),
+        M.Language(name='Goblin', script='Common'),
+        M.Language(name='Primordial', script='Barazhad'),
+        M.Language(name='Supernal', script='Supernal'),
+        M.Language(name='Abyssal', script='Barazhad'),
     ])
 
 
--- models.py
+++ models.py
@@ -1,10 +1,20 @@
 from sqlalchemy import (Column, Integer, String, Float, ForeignKey, Boolean,
-                        Enum)
+                        Enum, Date, Table, Index)
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.orderinglist import ordering_list
 
 DeclBase = declarative_base()
+
+STR = 'Strength'
+CON = 'Constitution'
+DEX = 'Dexterity'
+INT = 'Intelligence'
+WIS = 'Wisdom'
+CHA = 'Charisma'
+
+AbilityType = Enum(STR, CON, DEX, INT, WIS, CHA)
 
 
 class Base(DeclBase):
@@ -26,6 +36,27 @@ class Size(Base):
                    " e.g. 4 = 4x4")
 
 
+class Language(Base):
+    '''Languages'''
+    __tablename__ = 'language'
+
+    name = Column(String, primary_key=True, doc='Name of language')
+    script = Column(String, doc='Script language is written in')
+
+
+class DamageType(Base):
+    '''Damage types'''
+    __tablename__ = 'damagetype'
+
+    name = Column(String, primary_key=True,
+                  doc='Name of damage type')
+    description = Column(String, doc='detailed description of the damage type')
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
 class Alignment(Base):
     '''Alignments'''
     __tablename__ = 'alignment'
@@ -39,6 +70,14 @@ class Alignment(Base):
                     doc="Whether this alignment is lawful")
     chaotic = Column(Boolean, default=False,
                      doc="Whether this alignment is chaotic")
+
+
+race_effect = Table(
+    'race_effect',
+    Base.metadata,
+    Column('racename', String, ForeignKey('race.name'), primary_key=True),
+    Column('effectname', String, ForeignKey('effect.name'), primary_key=True),
+)
 
 
 class Race(Base):
@@ -63,6 +102,15 @@ class Race(Base):
     patron_deity = relationship('Deity',
                                 uselist=False,
                                 backref='patron_of_race')
+    effects = relationship('Effect', secondary=race_effect)
+
+
+class_effect = Table(
+    'class_effect',
+    Base.metadata,
+    Column('classname', String, ForeignKey('class.name'), primary_key=True),
+    Column('effectname', String, ForeignKey('effect.name'), primary_key=True),
+)
 
 
 class Class(Base):
@@ -79,11 +127,16 @@ class Class(Base):
                        'Striker',
                        'Defender'),
                   doc="Class's role in a party")
+    surges = Column(
+        Integer, default=7,
+        doc='Number of healing surges this class starts out with')
+    effects = relationship('Effect', secondary=class_effect)
 
-    def __init__(self, name, power_source, role):
+    def __init__(self, name, power_source, role, **kwargs):
         self.name = name
         self.power_source = power_source
         self.role = role
+        super(Class, self).__init__(**kwargs)
 
 
 class DeityDomain(Base):
@@ -126,9 +179,11 @@ class Deity(Base):
 
 
 class Character(Base):
-    __tablename__ = 'player'
+    __tablename__ = 'character'
 
     name = Column(String, primary_key=True, doc="Character's name")
+    is_player = Column(Boolean, default=False,
+                       doc='Whether this character is controlled by a player')
     racename = Column(String, ForeignKey('race.name'), default='Human',
                       doc="Character's race")
     classname = Column(String, ForeignKey('class.name'),
@@ -137,7 +192,7 @@ class Character(Base):
                     doc="Character's height")
     weight = Column(Integer,
                     doc="Character's weight (in lbs)")
-    gender = Column(String,
+    gender = Column(Enum('Female', 'Male', 'Indeterminate'),
                     doc="Character's gender")
     alignmentname = Column(String,
                            ForeignKey('alignment.name'),
@@ -169,10 +224,145 @@ class Character(Base):
     intelligence = Column(Integer, default=10,
                           doc="Character's intelligence score")
 
-    misc_init = Column(Integer, default=0)
+    platinum = Column(Integer, default=0, doc='Amount of platinum carried')
+    gold = Column(Integer, default=0, doc='Amount of gold carried')
+    silver = Column(Integer, default=0, doc='Amount of silver carried')
+    copper = Column(Integer, default=0, doc='Amount of copper carried')
+
+    __mapper_args__ = {
+        'polymorphic_on': is_player,
+    }
 
     # Relationships
     deity = relationship(Deity)
     class_ = relationship(Class)
     alignment = relationship(Alignment)
     race = relationship(Race)
+
+
+class Player(Character):
+    '''A player controlled character'''
+    __mapper_args__ = {
+        'polymorphic_identity': True
+    }
+
+    playername = Column(String, doc='Name of player who runs this character')
+    xp = Column(Integer, doc='Experience points accumulated')
+
+
+class NPC(Character):
+    '''A DM controlled character or monster'''
+    __mapper_args__ = {
+        'polymorphic_identity': False
+    }
+
+
+class Skill(Base):
+    '''Skills a character can have'''
+    __tablename__ = 'skill'
+
+    name = Column(String, primary_key=True, doc='Name of this skill')
+    ability = Column(AbilityType,
+                     doc='Ability score relevant to this skill')
+    armor_penalty = Column(
+        Boolean, default=False,
+        doc='Whether armor penalty can affect this skill')
+
+    def __init__(self, name, ability, armor_penalty=False):
+        self.name = name
+        self.ability = ability
+        self.armor_penalty = armor_penalty
+
+
+class Effect(Base):
+    '''Ongoing effects that apply to characters'''
+    __tablename__ = 'effect'
+
+    name = Column(String, primary_key=True,
+                  doc='Name of effect')
+
+    stats = relationship('Stat', innerjoin=True, lazy='joined',
+                         backref='effect')
+
+
+class Stat(Base):
+    '''Several stats make up an effect'''
+
+    __tablename__ = 'effectstat'
+
+    id = Column(Integer, primary_key=True, doc='Unique id of effect')
+    effectname = Column(String, ForeignKey('effect.name'),
+                        doc='Name of this effect this stat belongs to')
+    ability = Column(AbilityType, doc='Ability score affected')
+    ability_mod = Column(Integer, doc='Mod to ability score')
+    max_hp = Column(Integer, doc="Mod to maximum hitpoints")
+    initiative = Column(Integer, doc="Mod to initiative")
+    fortitude = Column(Integer, doc="Mod to fortitude")
+    will = Column(Integer, doc="Mod to will")
+    reflex = Column(Integer, doc="Mod to reflex")
+    armor_class = Column(Integer, doc="Mod to armor class")
+    speed = Column(Integer, doc="Mod to speed")
+    damage = Column(Integer, doc="Mod to damage")
+    damage_mult = Column(Float, doc="Multiplier for damage")
+    attack_roll = Column(Integer, doc="Mod to attack roll")
+    saving_throw = Column(Integer, doc="Mod to saving throw roll")
+
+    language = Column(String, ForeignKey('language.name'),
+                      doc="Language understanding bestowed")
+
+    skillname = Column(String, ForeignKey('skill.name'),
+                       doc="Name of skill modified")
+    skill_mod = Column(Integer, doc='Mod to skill amount')
+
+    damagetype_name = Column(String, ForeignKey('damagetype.name'),
+                             doc='Damage type resisted/weakened (null if any)')
+    damagetype_mod = Column(Integer, doc='Mod to damage type')
+
+    __table_args__ = (Index('effect_name_idx', effectname), )
+
+
+class CharacterEncounter(Base):
+    '''Links a player to an encounter in initiative order'''
+    __tablename__ = 'encounter_init'
+
+    charactername = Column(String, ForeignKey('character.name'),
+                           primary_key=True,
+                           doc='Character in initiative')
+    encounter_id = Column(Integer, ForeignKey('encounter.id'),
+                          primary_key=True,
+                          doc='Encounter this init roll is for')
+    init_score = Column(
+        Integer, doc='Initiative rolled for this encounter')
+    position = Column(Integer, doc='Current position in the initiative order')
+    second_wind = Column(Boolean, default=False,
+                         doc='Whether the character still has a '
+                             'second wind for this encounter.')
+
+    character = relationship('Character', innerjoin=True, lazy='joined')
+
+
+class Encounter(Base):
+    '''Represents a single encounter'''
+    __tablename__ = 'encounter'
+
+    id = Column(Integer, primary_key=True,
+                doc="Unique Encounter identifier")
+    name = Column(String, doc="Name of Encounter")
+    session_id = Column(Integer, ForeignKey('session.id'),
+                        doc='Session this encounter belongs to')
+
+    session = relationship('Session', backref='encounters')
+    init_order = relationship('CharacterEncounter',
+                              order_by='CharacterEncounter.position',
+                              lazy='joined',
+                              collection_class=ordering_list('position'))
+    initiative = association_proxy('init_order', 'character')
+
+
+class Session(Base):
+    '''Play sessions'''
+    __tablename__ = 'session'
+
+    id = Column(Integer, primary_key=True,
+                doc="Unique session identifier")
+    date = Column(Date, doc="date session occurred on")
